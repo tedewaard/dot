d677f6c04a390a4a3d88bf40cb16d6ee80120240
Trevor <tr.edewaard@gmail.com>
2022-11-24 04:33:24 +0000

    Removed the deprecated update_capabilties

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,7 +3,7 @@ local nnoremap = Remap.nnoremap
 
 
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require'lspconfig'.gopls.setup{
     capabilities = capabilities,
     on_attach = function()
