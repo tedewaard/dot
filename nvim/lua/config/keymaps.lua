94c7c2b772f486421786f66f6f23fda29c20df7c
Trevor <tr.edewaard@gmail.com>
2024-04-04 15:32:59 -0400

    Added vim keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,8 +2,13 @@
 
 -- This is set in root init.lua since it needs to be set before lazy
 -- vim.g.mapleader = " "
+
+-- Open netrw
 vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
 
+-- Move line up and down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 --Center after moving up or down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
